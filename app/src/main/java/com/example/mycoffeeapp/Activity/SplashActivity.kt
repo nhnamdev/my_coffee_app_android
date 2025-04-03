2cb109a88dec4e47bc5bb605e56dc6775d0a35ca
nam <22130173@st.hcmuaf.edu.vn>
2025-04-03 22:27:02 +0700

    fist

---
+++ app/src/main/java/com/example/mycoffeeapp/Activity/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mycoffeeapp.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mycoffeeapp.R
+import com.example.mycoffeeapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+
+        }
+    }
+}
